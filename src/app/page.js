270d9df76ad08c643dabeef134ac4708648ea294
Tchasinga jacques <tchasingajacques@gmail.com>
2024-05-30 12:01:57 +0200

    new code is on

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import { AnimatedTooltipPreview } from "./Page/AnimatedTooltipPreview";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    
+      <AnimatedTooltipPreview />  
     </main>
   );
 }
