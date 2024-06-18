c450a0855e607d2e9ee15061e516d5da925c0dec
Tchasinga jacques <tchasingajacques@gmail.com>
2024-06-18 12:29:24 +0200

    new commit is added now

--- src/app/Pages/Small.jsx
+++ src/app/Pages/Small.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Small() {
   return (
     <div className="text-white"> {/* Ensure this z-index is lower than Navbars */}
-            
+            <h1 className="uppercase text-4xl font-bold">cars design progress</h1>
     </div>
   );
 }
