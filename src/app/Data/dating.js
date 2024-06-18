03826a73133662586d29698e161c6df7eba4616d
Tchasinga jacques <tchasingajacques@gmail.com>
2024-06-18 13:33:48 +0200

    Node is added now

---
+++ src/app/Data/dating.js
@@ -0,0 +1,28 @@
+const managerdata = [
+    {
+        id : 1,
+        Concept : "Research & Analyze",
+        Lines :   <hr className="w-28"/>,
+        Mynumber : "01",
+    },
+    {
+        id : 2,
+        Concept : "Concept & Sketch",
+        Lines :   <hr className="w-28"/>,
+        Mynumber : "02",
+    },
+    {
+        id : 3,
+        Concept : "Desing & Brand",
+        Lines :   <hr className="w-28"/>,
+        Mynumber : "03",
+    },
+    {
+        id : 4,
+        Concept : "Market & Advertise",
+        Lines :   <hr className="w-28"/>,
+        Mynumber : "04",
+    }
+]
+
+export default managerdata;
