610d85810fbdc6c41f56adcdc7d5b4dbf68dc35e
Tchasinga jacques <tchasingajacques@gmail.com>
2024-06-18 10:16:57 +0200

    new node is added

--- src/app/Pages/Keeper.jsx
+++ src/app/Pages/Keeper.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import Oneimg from './../../app/Image/pexelsphoto.jpeg'; // Correctly specify the path to the image file
+import Navbars from './Navbars';
 
 export default function Keeper() {
   return (
     <div
-      className="h-96 bg-cover bg-center bg-no-repeat relative"
+      className="h-[550px] bg-cover bg-center bg-no-repeat relative w-full"
       style={{
         background: `linear-gradient(to right, rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0)), url(${Oneimg.src}) center / cover no-repeat`,
       }}
     >
-      <div className="max-w-5xl mx-auto">
-          <h1>Balolebwami Jack</h1>
+      <div className="max-w-5xl mx-auto w-full">
+          <Navbars/>
       </div>
     </div>
   );
---
+++ src/app/Pages/Navbars.jsx
@@ -0,0 +1,80 @@
+'use client';
+
+import React from 'react';
+import { useState } from 'react'; // Removed unused useEffect import
+
+export default function Navbars() {
+    const [open, setOpen] = useState(false);
+    const menuLinks = [
+        { name: "Home", link: "#home" },
+        { name: "About", link: "#about" },
+        { name: "Skills", link: "#skills" },
+        { name: "Portfolio", link: "#projects" },
+        { name: "Contact", link: "#contact" },
+    ];
+
+    return (
+        <nav className='fixed font-poppins font-sans w-full left-0 top-0 z-[999] text-gray-900'>
+            <div className="flex items-center justify-between">
+                <div className="mx-7">
+                    <h4 className="text-sm myheader uppercase font-bold">
+                        Tcha<span className="text-cyan-600">si</span>nga
+                    </h4>
+                </div>
+
+                <div className='text-gray-900 md:block hidden px-7 py-2 font-medium'>
+                    <ul className="flex items-center font-poppins font-sans gap-1 py-2 text-sm">
+                        {menuLinks.map((menu, i) => (
+                            <li key={i} className="px-6 font-poppins font-sans hover:text-cyan-600">
+                                <a href={menu.link}>{menu.name}</a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <div
+                    onClick={() => setOpen(!open)}
+                    className={`z-[999] ${open ? "text-gray-200" : "text-gray-100"} text-3xl md:hidden m-5`}
+                >
+                    <svg
+                        className="w-8 h-8 text-slate-200 myTrabs"
+                        fill="none"
+                        stroke="currentColor"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        {open ? (
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M6 18L18 6M6 6l12 12"
+                            ></path>
+                        ) : (
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M4 6h16M4 12h8m-8 6h16"
+                            ></path>
+                        )}
+                    </svg>
+                </div>
+                <div
+                    className={`md:hidden text-gray-900 absolute w-full h-screen px-7 py-2 font-medium bg-black top-0 duration-300 ${open ? "right-0" : "right-[-100%]"}`}
+                >
+                    <ul className="flex flex-col justify-center h-full text-white gap-10 py-2 text-lg">
+                        {menuLinks.map((menu, i) => (
+                            <li
+                                onClick={() => setOpen(false)}
+                                key={i}
+                                className="px-6 hover:text-cyan-600"
+                            >
+                                <a href={menu.link}>{menu.name}</a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+}
