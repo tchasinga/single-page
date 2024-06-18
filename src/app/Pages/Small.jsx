eff70cb6577753223bac598c7ccb4bffbd9fc1b3
Tchasinga jacques <tchasingajacques@gmail.com>
2024-06-18 13:08:44 +0200

    new changes

--- src/app/Pages/Small.jsx
+++ src/app/Pages/Small.jsx
@@ -5,11 +5,14 @@ import { RxColorWheel } from "react-icons/rx";
 export default function Small() {
   return (
     <div className="flex-col flex justify-center"> 
-             <div className="pb-2 flex gap-2">
+             <div className="pb-2 flex gap-2 flex-wrap">
              <h1 className="text-slate-300 uppercase text-4xl font-bold ">cars design</h1>
              <h1 className="text-slate-300 uppercase text-4xl font-bold flex items-center justify-center">pr<RxColorWheel className="spinerCode"/>gress</h1>
              </div>
             <hr className="w-28"/>
+            <div className="text-slate-300 pt-2 first-letter:uppercase text-sm leading-normal w-full">
+              <p className="">A car management system is a software solution designed to help organizations <br/> manage their fleet of vehicles efficiently</p>
+            </div>
     </div>
   );
 }
