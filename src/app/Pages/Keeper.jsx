0864fedf4a9d7794847cf948efca5957ee5cd82f
Tchasinga jacques <tchasingajacques@gmail.com>
2024-06-18 09:40:58 +0200

    new code is added

--- src/app/Pages/Keeper.jsx
+++ src/app/Pages/Keeper.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React from 'react';
+import Oneimg from "../Image/pexelsphoto.jpeg"; // Fixed import statement
 
 export default function Keeper() {
   return (
-    <div className="">
+    <div className="" style={{background: `linear-gradient(to right, rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0)), url(${Oneimg}) center / cover no-repeat`}}>
+        <h1>Balelobwami</h1>
     </div>
-  )
+  );
 }
