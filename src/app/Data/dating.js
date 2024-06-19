66b59d667a9af848bfb6e0526bbeb1424d404490
Tchasinga jacques <tchasingajacques@gmail.com>
2024-06-19 09:05:30 +0200

    new commit

--- src/app/Data/dating.js
+++ src/app/Data/dating.js
@@ -11,20 +11,21 @@ const managerdata = [
         Concept: "Concept & Sketch",
         Lines: <hr className="w-28" />,
         Mynumber: "02",
-        detailing : "Creating a concept and sketch for a website involves several key steps: defining the purpose, identifying the target audience, outlining the main features, and designing the layout. Here's a step-by-step guide to help you through the process."
+        detailing: "Creating a concept and sketch for a website involves several key steps: defining the purpose, identifying the target audience, outlining the main features, and designing the layout. Here's a step-by-step guide to help you through the process."
     },
     {
         id: 3,
         Concept: "Design & Brand",
         Lines: <hr className="w-28" />,
         Mynumber: "03",
-        detailing : ""
+        detailing: "Creating a successful UI design and brand identity involves multiple elements including color schemes, typography, imagery, icons, and overall visual style. Here is a step-by-step guide to help you design and brand your UI effectively:"
     },
     {
         id: 4,
         Concept: "Market & Advertise",
         Lines: <hr className="w-28" />,
         Mynumber: "04",
+        detailing: "Marketing and advertising are essential for any business to succeed. Here are some key steps to help you market and advertise your business effectively:"
     }
 ];
 
