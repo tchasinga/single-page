c3e681e490914cda3827fd3d7052f756ad1675b4
Tchasinga jacques <tchasingajacques@gmail.com>
2024-06-19 09:03:33 +0200

    new changes added now

--- src/app/Data/dating.js
+++ src/app/Data/dating.js
@@ -1,28 +1,31 @@
 const managerdata = [
     {
-        id : 1,
-        Concept : "Research & Analyze",
-        Lines :   <hr className="w-28"/>,
-        Mynumber : "01",
+        id: 1,
+        Concept: "Research & Analyze",
+        Lines: <hr className="w-28" />,
+        Mynumber: "01",
+        detailing: "Horse-Drawn Carriages to Automobiles: The first cars were essentially motorized carriages, and design was more about function than aesthetics. Streamlining 1920s 1930s Influenced by aviation and the concept of aerodynamics, cars started to adopt more streamlined shapes for better performance."
     },
     {
-        id : 2,
-        Concept : "Concept & Sketch",
-        Lines :   <hr className="w-28"/>,
-        Mynumber : "02",
+        id: 2,
+        Concept: "Concept & Sketch",
+        Lines: <hr className="w-28" />,
+        Mynumber: "02",
+        detailing : "Creating a concept and sketch for a website involves several key steps: defining the purpose, identifying the target audience, outlining the main features, and designing the layout. Here's a step-by-step guide to help you through the process."
     },
     {
-        id : 3,
-        Concept : "Desing & Brand",
-        Lines :   <hr className="w-28"/>,
-        Mynumber : "03",
+        id: 3,
+        Concept: "Design & Brand",
+        Lines: <hr className="w-28" />,
+        Mynumber: "03",
+        detailing : ""
     },
     {
-        id : 4,
-        Concept : "Market & Advertise",
-        Lines :   <hr className="w-28"/>,
-        Mynumber : "04",
+        id: 4,
+        Concept: "Market & Advertise",
+        Lines: <hr className="w-28" />,
+        Mynumber: "04",
     }
-]
+];
 
 export default managerdata;
