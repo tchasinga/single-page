049cf7e3c1ea9a49aaaad104312125c77c503161
Tchasinga jacques <tchasingajacques@gmail.com>
2024-06-19 10:36:41 +0200

    new code gaming now

--- src/app/Data/dating.js
+++ src/app/Data/dating.js
@@ -13,7 +13,7 @@ const managerdata = [
         Lines: <hr className="w-28" />,
         Mynumber: "02",
         detailing: "Creating a concept and sketch for a website involves several key steps: defining the purpose, identifying the target audience, outlining the main features, and designing the layout. Here's a step-by-step guide to help you through the process.",
-        bgGround: "https://images.pexels.com/photos/356056/pexels-photo-356056.jpeg?auto=compress&cs=tinysrgb&w=600",
+        bgGround: "https://images.pexels.com/photos/65438/pexels-photo-65438.jpeg?auto=compress&cs=tinysrgb&w=600",
     },
     {
         id: 3,
@@ -21,7 +21,7 @@ const managerdata = [
         Lines: <hr className="w-28" />,
         Mynumber: "03",
         detailing: "Creating a successful UI design and brand identity involves multiple elements including color schemes, typography, imagery, icons, and overall visual style. Here is a step-by-step guide to help you design and brand your UI effectively:",
-        bgGround: "https://images.pexels.com/photos/356056/pexels-photo-356056.jpeg?auto=compress&cs=tinysrgb&w=600",
+        bgGround: "https://images.pexels.com/photos/760714/pexels-photo-760714.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     },
     {
         id: 4,
@@ -29,7 +29,7 @@ const managerdata = [
         Lines: <hr className="w-28" />,
         Mynumber: "04",
         detailing: "Marketing and advertising are essential for any business to succeed. Here are some key steps to help you market and advertise your business effectively:",
-        bgGround: "https://images.pexels.com/photos/356056/pexels-photo-356056.jpeg?auto=compress&cs=tinysrgb&w=600",
+        bgGround: "https://images.pexels.com/photos/3165335/pexels-photo-3165335.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     }
 ];
 
--- src/app/Pages/Today.jsx
+++ src/app/Pages/Today.jsx
@@ -2,22 +2,22 @@ import React from 'react'
 import managerdata from '../Data/dating.js'
 
 export default function Today() {
-  return (
-    <div className="">
-         {
-            managerdata.map((people)=>(
-                <div className="">
-                    <div className="">
-                        <h1 className="text-slate-800 uppercase text-4xl font-bold ">{people.Concept}</h1>
-                        {people.Lines}
-                        <p className="text-sm text-slate-700">{people.detailing}</p>
+    return (
+        <div className="">
+            {
+                managerdata.map((people) => (
+                    <div className="flex items-center justify-between align-middle">
+                        <div className="">
+                            <h1 className="text-slate-800 uppercase text-4xl font-bold ">{people.Concept}</h1>
+                            {people.Lines}
+                            <p className="text-sm text-slate-700">{people.detailing}</p>
+                        </div>
+                        <div className="text-transparent bg-cover bg-clip-text" style={{ backgroundImage: `url(${people.bgGround})` }}>
+                            <h1 className="uppercase text-[300px] font-black">{people.Mynumber}</h1>
+                        </div>
                     </div>
-                    <div className="text-transparent bg-cover bg-clip-text" style={{backgroundImage: `url(${people.bgGround})`}}>
-    <h1 className="uppercase text-[300px] font-black">{people.Mynumber}</h1>
-</div>
-                </div>
-            ))
-         }
-    </div>
-  )
+                ))
+            }
+        </div>
+    )
 }
