0c7138f7da82ffb22b174ccb26f8bd0538f7aefb
Tchasinga jacques <tchasingajacques@gmail.com>
2024-06-19 13:24:05 +0200

    new fixing side is added now

--- src/app/Pages/Today.jsx
+++ src/app/Pages/Today.jsx
@@ -10,13 +10,13 @@ export default function Today() {
             {
                 managerdata.map((people, index) => (
                     <motion.div
-                        className="border-b-2 flex items-center justify-between align-middle reversingflex flex-wrap"
+                        className="border-b-2 flex items-center justify-between align-middle reversingflex"
                         initial={{ opacity: 0 }}
                         animate={{ opacity: 1 }}
                         transition={{ delay: index * 1 }}
                         key={index}
                     >
-                        <div>
+                        <div className="typographie">
                             <h1 className="text-slate-800 py-2 uppercase text-4xl font-bold">{people.Concept}</h1>
                             {people.Lines}
                             <p className="text-sm text-slate-700 pt-2 max-w-lg">{people.detailing}</p>
