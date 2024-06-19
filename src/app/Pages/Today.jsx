8a8e71c15733c70543c25195e524a3dc7febe438
Tchasinga jacques <tchasingajacques@gmail.com>
2024-06-19 10:03:32 +0200

    chore: Refactor Today component to use map function for rendering manager data

--- src/app/Pages/Today.jsx
+++ src/app/Pages/Today.jsx
@@ -3,8 +3,21 @@ import managerdata from '../Data/dating.js'
 
 export default function Today() {
   return (
-    <div>
-        <h1>Balolebwami</h1> 
+    <div className="">
+         {
+            managerdata.map((people)=>(
+                <div className="">
+                    <div className="">
+                        <h1 className="text-slate-800 uppercase text-4xl font-bold ">{people.Concept}</h1>
+                        {people.Lines}
+                        <p className="text-sm text-slate-700">{people.detailing}</p>
+                    </div>
+                    <div className="">
+                        <h1 className="text-slate-800 uppercase text-[300px] font-bold ">{people.Mynumber}</h1>
+                    </div>
+                </div>
+            ))
+         }
     </div>
   )
 }
