76c3813b36e00f50680ed0ba725d3b735ec0b97a
Tchasinga jacques <tchasingajacques@gmail.com>
2024-06-19 09:42:41 +0200

    new changes is added now

--- src/app/Pages/Today.jsx
+++ src/app/Pages/Today.jsx
@@ -4,7 +4,7 @@ import managerdata from '../Data/dating.js'
 export default function Today() {
   return (
     <div>
-        
+         
     </div>
   )
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import Keeper from "./Pages/Keeper";
+import Today from "./Pages/Today";
 
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
   return (
     <main className="">
       <Keeper/>
+      <Today/>
     </main>
   );
 }
